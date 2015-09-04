f82e0bf30d15818ee78957baf40f6707227fab23
Ivan Montiel <idmontie@asu.edu>
2015-09-04 09:15:38 -0700

    Rename feed facades to services

--- src/services/register.php
+++ src/services/register.php
@@ -2,6 +2,9 @@
 
 namespace Honeycomb\Services;
 
+/**
+ * Register class names for Hooks that should be run
+ */
 class Register {
   public function register( $classes ) {
     foreach( $classes as $class_name ) {
---
+++ tests/integration/wordpress-feed-service-test.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Honeycomb\Tests\Integration;
+
+use Honeycomb\Services\Wordpress_Feed_Service;
+
+/**
+ * Test the WordPress Feed Test
+ */
+class Wordpress_Feed_Test extends \PHPUnit_Framework_TestCase {
+  function setUp() {
+    $this->url     = 'https://news.wp.prod.gios.asu.edu/tag/board-letter/feed/';
+    $this->bad_url = 'invalid://news.wp.prod.gios.asu.nope/tag/board-letter/feed/';
+  }
+
+  function test_returns_rss_feed() {
+    $service = new Wordpress_Feed_Service();
+    $rss_feed = $service->get_feed( $this->url );
+
+    $this->assertInstanceOf( 'Nectary\Models\Rss_Feed', $rss_feed );
+  }
+
+  function test_returns_rss_feed_data() {
+    $service = new Wordpress_Feed_Service();
+    $rss_feed = $service->get_feed( $this->url );
+    $rss_feed->retrieve_items();
+
+    $items = $rss_feed->get_items();
+
+    $this->assertTrue( 0 < count( $items ) );
+  }
+
+  /**
+   * @expectedException \Exception
+   */
+  function test_throws_error_when_invalid() {
+    $service = new Wordpress_Feed_Service();
+    $rss_feed = $service->get_feed( $this->bad_url );
+
+    $rss_feed->retrieve_items();
+  }
+}
